7dfca701dcc12094cbee2edb7760545bae537eb3
nmougino <nmougino@student.42.fr>
2016-06-10 18:26:38 +0200

    Separation des itoa

---
+++ srcs/sitoa.c
@@ -0,0 +1,19 @@
+
+void	sitoa(intmax_t p, t_print *print, int l)
+{
+	char	c[l + 1];
+
+	c[l] = 0;
+	p = ft_abs(p);
+	while (l--)
+	{
+		c[l] = p % 10 + '0';
+		p /= 10;
+	}
+	l = 0;
+	while (c[l])
+	{
+		addto(c[l], print);
+		++l;
+	}
+}
---
+++ srcs/uitoabase.c
@@ -0,0 +1,20 @@
+
+void	uitoabase(uintmax_t p, char *base, t_print *print, int l)
+{
+	char	c[l + 1];
+	char	baselen;
+
+	baselen = ft_strlen(base);
+	c[l] = 0;
+	while (l--)
+	{
+		c[l] = base[p % baselen];
+		p /= baselen;
+	}
+	l = 0;
+	while (c[l])
+	{
+		addto(c[l], print);
+		++l;
+	}
+}

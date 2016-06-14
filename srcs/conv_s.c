f506fb5ac551fcb777c2727a106fb5e94d436f60
nmougino <nmougino@student.42.fr>
2016-06-14 17:54:03 +0200

    Resolve null string

--- srcs/conv_s.c
+++ srcs/conv_s.c
@@ -6,13 +6,23 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/12 19:05:54 by nmougino          #+#    #+#             */
-/*   Updated: 2016/06/12 19:05:56 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/06/14 17:53:10 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	conv_s(t_spec *spec, t_print *print)
+static void	convnull(t_print *print)
+{
+	addto('(', print);
+	addto('n', print);
+	addto('u', print);
+	addto('l', print);
+	addto('l', print);
+	addto(')', print);
+}
+
+void		conv_s(t_spec *spec, t_print *print)
 {
 	int		i;
 	int		neg;
@@ -25,9 +35,15 @@ void	conv_s(t_spec *spec, t_print *print)
 	}
 	i = 0;
 	s = (char *)urecupparam(E_LONG, print->ap);
-	neg = (spec->prec > -1 ? ft_min(spec->prec, ft_strlen(s)) : ft_strlen(s));
-	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw - neg) : 0;
-	while (s[i] && (spec->prec--))
-		addto(s[i++], print);
-	(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw) : 0;
+	if (s)
+	{
+		neg = (spec->prec > -1 ? ft_min(spec->prec, ft_strlen(s)) :
+				ft_strlen(s));
+		(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw - neg) : 0;
+		while (s[i] && (spec->prec--))
+			addto(s[i++], print);
+		(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw) : 0;
+	}
+	else
+		convnull(print);
 }

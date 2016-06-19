79a184043b0fd12a3e1cd2de82fe1c9beb9b617f
nmougino <nmougino@student.42.fr>
2016-06-19 20:40:56 +0200

    Fix unicode

--- srcs/ft_nbrlenbase.c
+++ srcs/ft_nbrlenbase.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/09 17:06:58 by nmougino          #+#    #+#             */
-/*   Updated: 2016/06/10 18:43:37 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/06/19 20:13:18 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 int		ft_nbrlenbase(unsigned long nb, int base)
 {
+	double	tmp;
 	int		i;
 
 	i = 1;
-	while (nb > (unsigned long)base)
+	tmp = (double)nb;
+	while (tmp > (unsigned long)base)
 	{
-		i++;
-		nb /= base;
+		++i;
+		tmp /= base;
 	}
 	return (i);
 }

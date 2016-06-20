005130f3b8f02e1eb7d815ae78dc8d9d072f82e4
nmougino <nmougino@student.42.fr>
2016-06-20 18:55:46 +0200

    Fix specifical behaviour on d i and o

--- srcs/conv_di.c
+++ srcs/conv_di.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/12 19:07:26 by nmougino          #+#    #+#             */
-/*   Updated: 2016/06/12 19:17:22 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/06/20 18:42:00 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,40 @@
 void	conv_i(t_spec *spec, t_print *print)
 {
 	int			i;
+	int			j;
 	intmax_t	p;
 
 	p = recupparam(spec->hljz, print->ap);
 	i = (p < 0) || (spec->flags & 3);
 	++spec->mfw;
+	//j = (!spec->prec && !p) ? 0
+	j = !(spec->prec || p) ? 0
+		: (ft_max(spec->prec + i, ft_nbrlen(ft_abs(p)) + i));
 	(spec->flags & E_ZERO) ? applyplusspace(print, spec, p >= 0) : --spec->mfw;
-	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw -
-		(ft_max(spec->prec + i, ft_nbrlen(ft_abs(p)) + i))) : 0;
+	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw - j) : 0;
 	!(spec->flags & E_ZERO) ? applyplusspace(print, spec, p >= 0) : 0;
 	applynumprec(print, spec, ft_nbrlen(ft_abs(p)));
-	sitoa(p, print, ft_nbrlen(ft_abs(p)));
+	(!spec->prec && !p) ? 0 : sitoa(p, print, ft_nbrlen(ft_abs(p)));
 	(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw) : 0;
 }
 
 void	conv_d(t_spec *spec, t_print *print)
 {
 	int			i;
+	int			j;
 	intmax_t	p;
 
 	p = recupparam(spec->hljz, print->ap);
 	i = (p < 0) || (spec->flags & 3);
 	++spec->mfw;
+	//j = (!spec->prec && !p) ? 0
+	j = !(spec->prec || p) ? 0
+		: (ft_max(spec->prec + i, ft_nbrlen(ft_abs(p)) + i));
 	(spec->flags & E_ZERO) ? applyplusspace(print, spec, p >= 0) : --spec->mfw;
-	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw -
-		(ft_max(spec->prec + i, ft_nbrlen(ft_abs(p)) + i))) : 0;
+	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw - j) : 0;
 	!(spec->flags & E_ZERO) ? applyplusspace(print, spec, p >= 0) : 0;
 	applynumprec(print, spec, ft_nbrlen(ft_abs(p)));
-	sitoa(p, print, ft_nbrlen(ft_abs(p)));
+	(!spec->prec && !p) ? 0 : sitoa(p, print, ft_nbrlen(ft_abs(p)));
 	(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw) : 0;
 }
 
--- srcs/conv_o.c
+++ srcs/conv_o.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/12 19:08:12 by nmougino          #+#    #+#             */
-/*   Updated: 2016/06/12 19:51:04 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/06/20 18:54:21 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,18 @@ void	conv_o(t_spec *spec, t_print *print)
 	int			i;
 	uintmax_t	p;
 	int			j;
+	int			k;
 
 	p = urecupparam(spec->hljz, print->ap);
 	i = (spec->flags & E_SHARP && !(spec->flags & E_ZERO));
 	(spec->flags & E_ZERO) ? applysharp(print, spec) : 0;
 	j = ((spec->prec == -1) ? i : 0);
-	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw -
-		(j + ft_max(spec->prec, ft_nbrlenbase(p, 8)))) : 0;
+	k = !(spec->prec || p) ? 0 : (j + ft_max(spec->prec, ft_nbrlenbase(p, 8)));
+	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw - k) : 0;
 	(!(spec->flags & E_ZERO)) ? applysharp(print, spec) : 0;
 	applynumprec(print, spec, i + ft_nbrlenbase(p, 8));
-	uitoabase(p, "01234567", print, ft_nbrlenbase(p, 8));
+	!(spec->prec || p) ? 0
+		: uitoabase(p, "01234567", print, ft_nbrlenbase(p, 8));
 	(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw) : 0;
 }
 

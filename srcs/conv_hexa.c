6f2466de4cb0e7df74ea83fc1422d8886e42d0a3
nmougino <nmougino@student.42.fr>
2016-06-20 17:52:58 +0200

    Fix hexa 0x0 for 0

--- srcs/conv_hexa.c
+++ srcs/conv_hexa.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/12 19:07:00 by nmougino          #+#    #+#             */
-/*   Updated: 2016/06/12 19:18:28 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/06/20 17:50:37 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ void	conv_x(t_spec *spec, t_print *print)
 
 	p = urecupparam(spec->hljz, print->ap);
 	i = ((spec->flags & E_SHARP && !(spec->flags & E_ZERO)) ? 2 : 0);
-	(spec->flags & E_ZERO) ? applysharp(print, spec) : 0;
+	(spec->flags & E_ZERO && p) ? applysharp(print, spec) : 0;
 	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw -
 		(i + ft_max(spec->prec, ft_nbrlenbase(p, 16)))) : 0;
-	(!(spec->flags & E_ZERO)) ? applysharp(print, spec) : 0;
+	(!(spec->flags & E_ZERO) && p) ? applysharp(print, spec) : 0;
 	applynumprec(print, spec, ft_nbrlenbase(p, 16));
 	uitoabase(p, "0123456789abcdef", print, ft_nbrlenbase(p, 16));
 	(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw) : 0;
@@ -35,10 +35,10 @@ void	conv_bx(t_spec *spec, t_print *print)
 
 	p = urecupparam(spec->hljz, print->ap);
 	i = ((spec->flags & E_SHARP && !(spec->flags & E_ZERO)) ? 2 : 0);
-	(spec->flags & E_ZERO) ? applysharp(print, spec) : 0;
+	(spec->flags & E_ZERO && p) ? applysharp(print, spec) : 0;
 	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw -
 		(i + ft_max(spec->prec, ft_nbrlenbase(p, 16)))) : 0;
-	(!(spec->flags & E_ZERO)) ? applysharp(print, spec) : 0;
+	(!(spec->flags & E_ZERO) && p) ? applysharp(print, spec) : 0;
 	applynumprec(print, spec, ft_nbrlenbase(p, 16));
 	uitoabase(p, "0123456789ABCDEF", print, ft_nbrlenbase(p, 16));
 	(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw) : 0;

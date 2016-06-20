24a093ae52cee99d1639caa837f3ebad466ea7e8
nmougino <nmougino@student.42.fr>
2016-06-20 23:46:06 +0200

    Fix 0 + prec in di

--- srcs/conv_di.c
+++ srcs/conv_di.c
@@ -6,30 +6,11 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/12 19:07:26 by nmougino          #+#    #+#             */
-/*   Updated: 2016/06/20 18:56:37 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/06/20 23:45:44 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	conv_i(t_spec *spec, t_print *print)
-{
-	int			i;
-	int			j;
-	intmax_t	p;
-
-	p = recupparam(spec->hljz, print->ap);
-	i = (p < 0) || (spec->flags & 3);
-	++spec->mfw;
-	j = !(spec->prec || p) ? 0
-		: (ft_max(spec->prec + i, ft_nbrlen(ft_abs(p)) + i));
-	(spec->flags & E_ZERO) ? applyplusspace(print, spec, p >= 0) : --spec->mfw;
-	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw - j) : 0;
-	!(spec->flags & E_ZERO) ? applyplusspace(print, spec, p >= 0) : 0;
-	applynumprec(print, spec, ft_nbrlen(ft_abs(p)));
-	(!spec->prec && !p) ? 0 : sitoa(p, print, ft_nbrlen(ft_abs(p)));
-	(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw) : 0;
-}
 
 void	conv_d(t_spec *spec, t_print *print)
 {
@@ -39,15 +20,36 @@ void	conv_d(t_spec *spec, t_print *print)
 
 	p = recupparam(spec->hljz, print->ap);
 	i = (p < 0) || (spec->flags & 3);
-	++spec->mfw;
 	j = !(spec->prec || p) ? 0
 		: (ft_max(spec->prec + i, ft_nbrlen(ft_abs(p)) + i));
+	++spec->mfw;
 	(spec->flags & E_ZERO) ? applyplusspace(print, spec, p >= 0) : --spec->mfw;
+	(spec->flags & E_ZERO && p > 0) ? --spec->prec : 0;
 	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw - j) : 0;
 	!(spec->flags & E_ZERO) ? applyplusspace(print, spec, p >= 0) : 0;
 	applynumprec(print, spec, ft_nbrlen(ft_abs(p)));
 	(!spec->prec && !p) ? 0 : sitoa(p, print, ft_nbrlen(ft_abs(p)));
-	(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw) : 0;
+	(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw + (p > 0)) : 0;
+}
+
+void	conv_i(t_spec *spec, t_print *print)
+{
+	int			i;
+	int			j;
+	intmax_t	p;
+
+	p = recupparam(spec->hljz, print->ap);
+	i = (p < 0) || (spec->flags & 3);
+	j = !(spec->prec || p) ? 0
+		: (ft_max(spec->prec + i, ft_nbrlen(ft_abs(p)) + i));
+	++spec->mfw;
+	(spec->flags & E_ZERO) ? applyplusspace(print, spec, p >= 0) : --spec->mfw;
+	(spec->flags & E_ZERO && p > 0) ? --spec->prec : 0;
+	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw - j) : 0;
+	!(spec->flags & E_ZERO) ? applyplusspace(print, spec, p >= 0) : 0;
+	applynumprec(print, spec, ft_nbrlen(ft_abs(p)));
+	(!spec->prec && !p) ? 0 : sitoa(p, print, ft_nbrlen(ft_abs(p)));
+	(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw + (p > 0)) : 0;
 }
 
 void	conv_ld(t_spec *spec, t_print *print)

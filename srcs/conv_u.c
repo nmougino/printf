5150baeda40da1315c825e42f744a159bad74fb7
nmougino <nmougino@student.42.fr>
2016-08-02 13:45:36 +0200

    Solve null long char

--- srcs/conv_u.c
+++ srcs/conv_u.c
@@ -6,7 +6,7 @@
 /*   By: nmougino <nmougino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/12 19:08:56 by nmougino          #+#    #+#             */
-/*   Updated: 2016/06/12 19:17:54 by nmougino         ###   ########.fr       */
+/*   Updated: 2016/08/02 13:40:33 by nmougino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@ void	conv_u(t_spec *spec, t_print *print)
 	uintmax_t	p;
 
 	p = urecupparam(spec->hljz, print->ap);
-	(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw -
-		(ft_max(spec->prec, ft_nbrlenbase(p, 10)))) : 0;
-	applynumprec(print, spec, ft_nbrlenbase(p, 10));
-	uitoabase(p, "0123456789", print, ft_nbrlenbase(p, 10));
-	(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw) : 0;
+	if (!(!p && !spec->prec))
+	{
+		(!(spec->flags & E_DASH)) ? applymfw(print, spec, spec->mfw -
+			(ft_max(spec->prec, ft_nbrlenbase(p, 10)))) : 0;
+		applynumprec(print, spec, ft_nbrlenbase(p, 10));
+		uitoabase(p, "0123456789", print, ft_nbrlenbase(p, 10));
+		(spec->flags & E_DASH) ? applymfw(print, spec, spec->mfw) : 0;
+	}
 }
 
 void	conv_lu(t_spec *spec, t_print *print)
